6edd9c622100e5b5cf9dc4566a846e65fe7c82cb
TwoTeaToTwo <66165270+TwoTeaToTwo@users.noreply.github.com>
2023-10-14 03:11:29 +0300

    Move Electron logic from bp to cpp

--- Source/SELF/Private/ParticalBase.cpp
+++ Source/SELF/Private/ParticalBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ParticalBase.h"
+#include "HiddenWall.h"
 
 // Sets default values
 AParticalBase::AParticalBase()
@@ -9,13 +9,18 @@ AParticalBase::AParticalBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+
+	SphereCollision->SetupAttachment(RootComponent);
+	SphereCollision->SetGenerateOverlapEvents(true);
 }
 
 // Called when the game starts or when spawned
 void AParticalBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SphereCollision->OnComponentBeginOverlap.AddDynamic(this, &AParticalBase::OnOverlapBegin);
 }
 
 // Called every frame
@@ -23,5 +28,24 @@ void AParticalBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	AddMovement(Speed);
 }
 
+void AParticalBase::AddMovement(float LSpeed)
+{
+	FVector DeltaLocation;
+
+	DeltaLocation.X = GetWorld()->GetDeltaSeconds() * LSpeed;
+
+	SphereCollision->AddWorldOffset(DeltaLocation, true);
+}
+
+void AParticalBase::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	AHiddenWall *HiddenWall = Cast<AHiddenWall>(OtherActor);
+
+	if (HiddenWall != nullptr)
+	{
+		this->Destroy();
+	}
+}
--- Source/SELF/Public/ParticalBase.h
+++ Source/SELF/Public/ParticalBase.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/SphereComponent.h"
 #include "ParticalBase.generated.h"
+
 
 UCLASS()
 class SELF_API AParticalBase : public APawn
@@ -18,6 +20,18 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USphereComponent *SphereCollision;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float Speed;
+
+	UFUNCTION()
+	void AddMovement(float LSpeed);
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 public:	
 	// Called every frame
